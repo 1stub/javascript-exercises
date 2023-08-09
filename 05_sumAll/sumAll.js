90260bb899f28485e1ed01fb4efa141b06c663c5
Anthony Arnold <stubbiegaming@gmail.com>
2023-08-09 14:48:28 -0400

    solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+let sum = 0;
+let max = 0;
+let min = 0;
 
+const sumAll = function(num1, num2) {
+    if (num1 > num2) {
+        max = num1;
+        min = num2;
+    }
+    else{
+        max = num2;
+        min = num1;
+    }
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return "ERROR";
+    }
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    return (max*(max+1) - (min-1)*min) / 2
 };
 
 // Do not edit below this line
