b952bc87a9de5b05441080c11e5d9f0113afd306
Anthony Arnold <stubbiegaming@gmail.com>
2023-08-09 12:11:21 -0400

    finished 01-04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reverseStr = [];
+    for(let i = string.length; i >= 0; i--) {
+        reverseStr.push(string[i]);
+    }
+    return reverseStr.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for(let i = arr.length - 1; i >= 0; i--){
+        for(let x = args.length - 1; x >= 0; x--){
+            if(arr[i] === args[x]){
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
