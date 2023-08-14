ad5b1be409a7ad149e80be36613d756aed90db95
Anthony Arnold <stubbiegaming@gmail.com>
2023-08-14 12:16:13 -0400

    solved 08-09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let newStr = str.replace(/[^\p{L}\p{N}\s]/gu, '');
+    newStr = newStr.replace(/\s/g, "");
+   let newArr = Array.from(newStr);
+   if(newArr.toString() === newArr.reverse().toString()) return true;
+   return false;
 };
 
 // Do not edit below this line
